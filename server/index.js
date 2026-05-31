cf6093c255e6f98d86434b8e763911dda3fcf8fc
Rayan <bainanceb8@gmail.com>
2026-05-31 14:21:36 +0300

    fix cors for vercel

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Fix CORS
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://job-tracker2-jade.vercel.app"],
   credentials: true,
 }));
 

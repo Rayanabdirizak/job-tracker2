53f8f0098f762360ff68fb8f332975caba99019e
Rayan <bainanceb8@gmail.com>
2026-05-31 14:38:35 +0300

    fix job model

--- server/models/jobs.js
+++ server/models/jobs.js
@@ -2,12 +2,35 @@ const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema(
   {
-    company: String,
-    position: String,
+    company: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    position: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     status: {
       type: String,
-      default: "Pending"
-    }
+      enum: ["Pending", "Interview", "Declined", "Accepted"],
+      default: "Pending",
+    },
+    jobLocation: {
+      type: String,
+      default: "Remote",
+    },
+    jobType: {
+      type: String,
+      enum: ["Full-time", "Part-time", "Contract", "Internship"],
+      default: "Full-time",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 );
